a2809ce4d315974eda137a348f9d99ab52c800ee
Renato de Andrade Soares <renato.andrade@opus-software.com.br>
2022-09-06 15:30:29 -0300

    feat: add storybook

---
+++ libs/react-components/.storybook/main.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const rootMain = require('../../../.storybook/main');
+
+module.exports = {
+  ...rootMain,
+
+  core: { ...rootMain.core, builder: 'webpack5' },
+
+  stories: [
+    ...rootMain.stories,
+    '../src/lib/**/*.stories.mdx',
+    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
+  webpackFinal: async (config, { configType }) => {
+    // apply any global webpack configs that might have been specified in .storybook/main.js
+    if (rootMain.webpackFinal) {
+      config = await rootMain.webpackFinal(config, { configType });
+    }
+
+    config.resolve.extensions.push('.css');
+
+    config.module.rules.push({
+      test: /\.s[ac]ss$/i,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../src/lib'),
+    });
+
+    // add your own webpack tweaks if needed
+
+    return config;
+  },
+};
