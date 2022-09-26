264f19ac255a99b5a58e473436d45c15f29263bc
Renato de Andrade Soares <renato.andrade@opus-software.com.br>
2022-09-26 16:22:30 -0300

    refactor: remove unnecessary files

--- libs/design-tokens/src/tasks/platforms/_tokens.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs-extra');
-const path = require('path');
-const tokens = fs
-  .readdirSync(path.resolve(__dirname, '../../lib/tokens'))
-  .map((file) => file.split('.')[0]);
-
-module.exports = tokens;
--- libs/web-components/.storybook/manager.js
+++
@@ -1,8 +0,0 @@
-import { addons } from '@storybook/addons';
-import webComponentsTheme from './webComponentsTheme';
-
-addons.setConfig({
-  theme: webComponentsTheme,
-  panelPosition: 'right',
-  enableShortcuts: false
-});
