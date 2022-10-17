33119ff76d62d31a206218b3a616ded3920f7680
Renato Andrade <renato.andrade@opus-software.com.br>
2022-10-17 18:22:18 -0300

    feat: define web test runner configuration

---
+++ web-test-runner.config.mjs
@@ -0,0 +1,22 @@
+import { playwrightLauncher } from '@web/test-runner-playwright'
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { fileURLToPath } from 'url';
+
+export default {
+  rootDir: 'packages/components/src',
+  files: ['packages/components/src/**/*.spec.ts'],
+  concurrentBrowsers: 3,
+  nodeResolve: true,
+  browsers: [
+    playwrightLauncher({ product: 'chromium' }),
+    playwrightLauncher({ product: 'firefox' }),
+    playwrightLauncher({ product: 'webkit' })
+  ],
+  plugins: [
+    esbuildPlugin({
+      ts: true,
+      target: 'auto',
+      tsconfig:  fileURLToPath(new URL('./packages/components/tsconfig.json', import.meta.url)),
+    })
+  ],
+};
