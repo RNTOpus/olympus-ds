3397f87b5edf8ad4fcafc212a57c010604ad6eb4
Renato Andrade <renato.andrade@opus-software.com.br>
2022-10-21 14:11:57 -0300

    refactor: remove unnecessary codes

--- packages/button/src/button.types.ts
+++
@@ -1,3 +0,0 @@
-export type OdsButtonVariant = 'primary' | 'success' | 'neutral' | 'warning' | 'danger'
-export type OdsButtonSize = 'small' | 'medium' | 'normal'
-export type OdsButtonType = 'button' | 'submit' | 'reset'
