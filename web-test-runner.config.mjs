0673256b63360da290277ea29d2c45283a7addf5
Renato Andrade <renato.andrade@opus-software.com.br>
2023-01-26 12:42:32 -0300

    ci: test

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,6 +1,6 @@
 import { playwrightLauncher } from '@web/test-runner-playwright'
-import { esbuildPlugin } from '@web/dev-server-esbuild';
-import { fileURLToPath } from 'url';
+import { esbuildPlugin } from '@web/dev-server-esbuild'
+import { fileURLToPath } from 'url'
 
 const jenkins_path = process.env.WORKSPACE
 
@@ -11,9 +11,13 @@ export default {
   nodeResolve: true,
   playwright: true,
   browsers: [
-    playwrightLauncher({ product: 'chromium', launchOptions: {
-      executablePath: `${jenkins_path}/node_modules/playwright-core/.local-browsers/chromium-1005/chrome-linux/chrome`
-    } }),
+    playwrightLauncher({
+      product: 'chromium',
+      launchOptions: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        executablePath: `${jenkins_path}/node_modules/playwright-core/.local-browsers/chromium-1005/chrome-linux/chrome`
+      }
+    }),
     playwrightLauncher({ product: 'firefox' }),
     playwrightLauncher({ product: 'webkit' })
   ],
@@ -21,7 +25,7 @@ export default {
     esbuildPlugin({
       ts: true,
       target: 'auto',
-      tsconfig:  fileURLToPath(new URL('./packages/components/tsconfig.json', import.meta.url)),
+      tsconfig: fileURLToPath(new URL('./packages/components/tsconfig.json', import.meta.url))
     })
-  ],
-};
+  ]
+}
